e9af4e1a3c55f4a3e938549a23bfed129c56284f
wendrewshay <wendrewshay@sina.com>
2017-03-19 16:36:13 +0800

    添加了ExampleExecutor

---
+++ src/main/java/com/xwq/dao/MyExampleRepository.java
@@ -0,0 +1,9 @@
+package com.xwq.dao;
+
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+
+import com.xwq.domain.Person2;
+
+public interface MyExampleRepository extends QueryByExampleExecutor<Person2>{
+
+}
